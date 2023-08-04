305ae416c38930edae77582e383f7521fcbbb912
ryhara <aaxouei52@gmail.com>
2023-08-05 00:03:34 +0900

    [Fix] comment min

--- srcs/sort_many.c
+++ srcs/sort_many.c
@@ -16,60 +16,32 @@
 void sort_all(t_stack *stack)
 {
 	size_t	size_b;
-	// t_node	*max;
-	// t_node	*min;
-	// t_node	*tmp;
-
-	// max = get_max(stack->head_b);
-	// min = get_min(stack->head_b);
-	// tmp = stack->head_b->next;
 	size_b = get_stack_size(stack->head_b);
 	while (size_b > SORT_SIZE)
 	{
-		// if (tmp->index == min->index)
-		// {
-		// 	pa(stack->head_a, stack->head_b);
-		// 	ra(stack->head_a);
-		// 	min = get_next_min(stack->head_b, min);
-		// }
-		// else if (tmp->index > (size_b / 2))
-		// 	pa(stack->head_a, stack->head_b);
-		// tmp = stack->head_b->next;
 		pa_harf(stack);
+		// 常にsizeを測りながら最小値を下に送っていくことにする。
 		stack_print(stack);
 		size_b = get_stack_size(stack->head_b);
-		// printf("%ld %ld\n", size_b, min->index);
 	}
+	sort_3(stack->head_a);
+	pa(stack->head_a, stack->head_b);
+	ra(stack->head_a);
+	pa(stack->head_a, stack->head_b);
+	ra(stack->head_a);
+	pa(stack->head_a, stack->head_b);
+	ra(stack->head_a);
 	stack_print(stack);
 }
 
 void	sort_many(t_stack *stack)
 {
 	t_node	*head_a;
-	// t_node	*min;
-	// t_node	*head_b;
-	// size_t	size;
-	// t_node	*tmp;
 
 	head_a = stack->head_a;
-	// head_b = stack->head_b;
-	// size = stack->size;
 	if (check_sorted(head_a))
 		return ;
-	// int pos = 50;
 	pb_harf(stack);
-	// while (stack->head_b->next != stack->head_b)
-	// {
-	// 	min = get_min(stack->head_b);
-	// 	pa_min_and_ra(stack, min);
-	// }
-	// while (pos--)
-	// 	pb(stack->head_a, stack->head_b);
-	// while (stack->head_b->next != stack->head_b)
-	// {
-	// 	min = get_min(stack->head_b);
-	// 	pa_min_and_ra(stack, min);
-	// }
 	sort_all(stack);
 }
 
@@ -99,6 +71,7 @@ void	pa_harf(t_stack *stack)
 {
 	t_node	*tmp;
 	t_node	*max;
+	t_node	*min;
 	size_t	size;
 	size_t	count;
 
@@ -109,8 +82,16 @@ void	pa_harf(t_stack *stack)
 	while (count <= (size / 2) - 1)
 	{
 		tmp = stack->head_b->next;
+		size = get_stack_size(stack->head_b);
 		max = get_max(stack->head_b);
-		if (tmp->index > (size / 2) && max == tmp)
+		min = get_min(stack->head_b);
+		if (tmp->index == min->index)
+		{
+			pa(stack->head_a, stack->head_b);
+			ra(stack->head_a);
+			count++;
+		}
+		else if (tmp->index > (size / 2) && max == tmp)
 		{
 			pa(stack->head_a, stack->head_b);
 			count++;
