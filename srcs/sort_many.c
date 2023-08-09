8bd0241ed480264c4f702951bf7379c7aaf56f98
ryhara <aaxouei52@gmail.com>
2023-08-09 20:36:11 +0900

    [Finish] sort_many 1

--- srcs/sort_many.c
+++ srcs/sort_many.c
@@ -58,6 +58,7 @@ t_node	*insert_count(t_stack *stack)
 {
 	t_node	*tmp_a;
 	t_node	*tmp_b;
+	t_node	*ans;
 	t_node	*min_a;
 	size_t	count_a;
 	size_t	count_b;
@@ -106,149 +107,18 @@ t_node	*insert_count(t_stack *stack)
 			}
 		}
 		if (count_min > count_a + count_b)
+		{
 			count_min = count_a + count_b;
+			ans = tmp_b;
+		}
 		if (count_min <= 2)
+		{
+			ans = tmp_b;
 			break ;
-		if (tmp_b->next == stack->head_b)
-			break ;
-		// printf("index %ld count %ld\n", tmp_b->index, count_min);
+		}
 		tmp_b = tmp_b->next;
 	}
-	// printf("[ANS] index %ld count %ld\n", tmp_b->index, count_min);
-	return (tmp_b);
-}
-
-t_node	*insert_count_middle(t_stack *stack)
-{
-	t_node	*tmp_a;
-	t_node	*tmp_b;
-	t_node	*min_a;
-	size_t	count_a;
-	size_t	count_b;
-	size_t	count_min;
-
-	tmp_b = stack->head_b->next;
-	count_min = stack->size;
-	min_a = get_min(stack->head_a);
-	stack->size_a = get_stack_size(stack->head_a);
-	stack->size_b = get_stack_size(stack->head_b);
-	while (tmp_b != stack->head_b)
-	{
-		while (tmp_b->index < stack->size / 3)
-			tmp_b = tmp_b->next;
-		tmp_a = stack->head_a->next;
-		count_b = get_pos(stack->head_b, tmp_b);
-		if (count_b > stack->size_b / 2)
-			count_b = stack->size_b - count_b;
-		count_a = 0;
-		if (tmp_b->index < min_a->index)
-		{
-			while (tmp_a->index != min_a->index)
-			{
-				tmp_a = tmp_a->next;
-				count_a++;
-			}
-			if (count_a > stack->size_a / 2)
-				count_a = stack->size_a - count_a;
-		}
-		else
-		{
-			if (tmp_a->index > tmp_b->index && stack->head_a->prev->index < tmp_b->index)
-				count_a++;
-			else
-			{
-				while (tmp_a->index > tmp_b->index)
-				{
-					tmp_a = tmp_a->next;
-					count_a++;
-				}
-				while (tmp_a->index < tmp_b->index)
-				{
-					tmp_a = tmp_a->next;
-					count_a++;
-				}
-				if (count_a > stack->size_a / 2)
-					count_a = stack->size_a - count_a;
-			}
-		}
-		if (count_min > count_a + count_b)
-			count_min = count_a + count_b;
-		if (count_min <= 2)
-			break ;
-		if (tmp_b->next == stack->head_b)
-			break ;
-		// printf("index %ld count %ld\n", tmp_b->index, count_min);
-		tmp_b = tmp_b->next;
-	}
-	// printf("[ANS] index %ld count %ld\n", tmp_b->index, count_min);
-	return (tmp_b);
-}
-
-t_node	*insert_count_big(t_stack *stack)
-{
-	t_node	*tmp_a;
-	t_node	*tmp_b;
-	t_node	*min_a;
-	size_t	count_a;
-	size_t	count_b;
-	size_t	count_min;
-
-	tmp_b = stack->head_b->next;
-	count_min = stack->size;
-	min_a = get_min(stack->head_a);
-	stack->size_a = get_stack_size(stack->head_a);
-	stack->size_b = get_stack_size(stack->head_b);
-	while (tmp_b != stack->head_b)
-	{
-		while (tmp_b->index < stack->size / 3 * 2)
-			tmp_b = tmp_b->next;
-		tmp_a = stack->head_a->next;
-		count_b = get_pos(stack->head_b, tmp_b);
-		if (count_b > stack->size_b / 2)
-			count_b = stack->size_b - count_b;
-		count_a = 0;
-		if (tmp_b->index < min_a->index)
-		{
-			while (tmp_a->index != min_a->index)
-			{
-				tmp_a = tmp_a->next;
-				count_a++;
-			}
-			if (count_a > stack->size_a / 2)
-				count_a = stack->size_a - count_a;
-		}
-		else
-		{
-			if (tmp_a->index > tmp_b->index && stack->head_a->prev->index < tmp_b->index)
-				count_a++;
-			else
-			{
-				while (tmp_a->index > tmp_b->index)
-				{
-					tmp_a = tmp_a->next;
-					count_a++;
-				}
-				while (tmp_a->index < tmp_b->index)
-				{
-					tmp_a = tmp_a->next;
-					count_a++;
-				}
-				if (count_a > stack->size_a / 2)
-					count_a = stack->size_a - count_a;
-			}
-		}
-		if (count_min > count_a + count_b)
-			count_min = count_a + count_b;
-		if (count_min <= 2)
-			break ;
-		if (tmp_b->next == stack->head_b)
-			break ;
-		printf("index %ld count %ld\n", tmp_b->index, count_min);
-		stack_print(stack);
-		tmp_b = tmp_b->next;
-	}
-	// printf("[ANS] index %ld count %ld\n", tmp_b->index, count_min);
-	return (tmp_b);
+	return (ans);
 }
 
 void	insert_new(t_stack *stack, t_node *node)
@@ -297,7 +167,9 @@ void	insert_new(t_stack *stack, t_node *node)
 			tmp_a = tmp_a->next;
 		count_a = get_pos(stack->head_a, tmp_a);
 		stack->size_a = get_stack_size(stack->head_a);
-		if (count_a > stack->size_a / 2)
+		if (stack->head_a->next->index > node->index && stack->head_a->prev->index < node->index)
+			;
+		else if (count_a > stack->size_a / 2)
 		{
 			while (1)
 			{
@@ -322,6 +194,7 @@ void	insert_new(t_stack *stack, t_node *node)
 void	sort_many(t_stack *stack)
 {
 	t_node	*head_a;
+	t_node	*min_a;
 	t_node	*tmp_b;
 	size_t	count;
 
@@ -331,29 +204,12 @@ void	sort_many(t_stack *stack)
 	pa_remain_max_five(stack);
 	sort_5(stack);
 	stack->size_b = get_stack_size(stack->head_b);
-	while (stack->size_b > stack->size / 3 * 2)
-	{
-		tmp_b = insert_count_big(stack);
-		insert_new(stack, tmp_b);
-		printf("FILE_NAME %s, LINE %d\n", __FILE__, __LINE__);
-		stack_print(stack);
-		stack->size_b = get_stack_size(stack->head_b);
-	}
-	while (stack->size_b > stack->size / 3 + 1)
-	{
-		tmp_b = insert_count_middle(stack);
-		insert_new(stack, tmp_b);
-		printf("FILE_NAME %s, LINE %d\n", __FILE__, __LINE__);
-		stack_print(stack);
-		stack->size_b = get_stack_size(stack->head_b);
-	}
 	while (stack->size_b != 0)
 	{
 		tmp_b = insert_count(stack);
 		insert_new(stack, tmp_b);
 		stack->size_b = get_stack_size(stack->head_b);
 	}
-	t_node	*min_a;
 	min_a = get_min(stack->head_a);
 	count = get_pos(stack->head_a, min_a);
 	if (count > stack->size / 2)
@@ -366,5 +222,4 @@ void	sort_many(t_stack *stack)
 		while (stack->head_a->next != min_a)
 			ra(stack->head_a);
 	}
-	// stack_print(stack);
 }
